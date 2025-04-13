dc39880ea2bcf7e8b156e47a1cde1eef64a5eb98
igobyjac <136859155+igobyjac@users.noreply.github.com>
2025-04-13 18:57:24 -0500

    ws startup

--- turtle/startup.lua
+++ turtle/startup.lua
@@ -1,1 +1,20 @@
 --                  This is the script that must be inputted into the Turtle to establish the connection to the WebSocket                    --
+
+local socket = "ws://localhost:5757"
+
+local ws, err = http.websocket(socket)
+
+if ws then
+    print('connection estasblished')
+
+    local dataOut = {
+        message = "Hello world - turtle"
+    }
+
+    local jsonData = textutils.serializeJSON(dataOut)
+    ws.send(jsonData)
+
+    ws.close()
+else
+    print('error: ' .. err)
+end
