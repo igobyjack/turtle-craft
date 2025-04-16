47d2c14bfc5e4961e17a646a1ad28867909a217a
igobyjac <136859155+igobyjac@users.noreply.github.com>
2025-04-16 00:19:38 -0500

    startup work

--- turtle/startup.lua
+++ turtle/startup.lua
@@ -1,27 +1,39 @@
 --                  This is the script that must be inputted into the Turtle to establish the connection to the WebSocket                    --
 
-os.loadAPI("json")
 -- NGROK url --
-local ws, err = http.WebSocket("wss://placeholder.net:5757")
+local ws, err = http.websocket("wss//:<NGROK_URL>")
 
-if err then
-        print("ws error: " .. err)
+-- planned JSON format: 
 
-else if ws then
+if ws then
         print("> connected")
         while true do
-                local message = ws.recieve()
-                print("recieved: " .. ws)
-
-                local obj = json.decode(message)
-                if obj.type == 'eval' then
+                local message = ws.receive()
+                print("received raw message: " .. tostring(message))
+            
+                local obj = textutils.unserializeJSON(message)
+                
+                if obj == nil then
+                    print("Failed to parse JSON.")
+                else
+                    print("Parsed JSON.")
+            
+                    if obj.type == "eval" then
                         if obj.command == "forward" then
-                                turtle.forward()
+                            turtle.forward()
                         elseif obj.command == "back" then
-                                turtle.back()
+                            turtle.back()
                         elseif obj.command == "dig" then
-                                turtle.dig()
+                            turtle.dig()
+                        else
+                            print("Unknown command: " .. tostring(obj.command))
                         end
+                    else
+                        print("Unknown message type: " .. tostring(obj.type))
+                    end
                 end
-        end
+            end
+            
+else
+        print("> error: " .. err)
 end
