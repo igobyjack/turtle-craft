7e0219ffc788c533ce3f3339aa6f12eb0cf936b3
igobyjac <136859155+igobyjac@users.noreply.github.com>
2025-04-18 19:06:20 -0500

    lua script of doom and hopelessness

--- turtle/startup.lua
+++ turtle/startup.lua
@@ -3,74 +3,45 @@
 -- NGROK url --
 local ws, err = http.websocket("wss://<NGROK_URL>")
 
--- JSON in fromat of { "type": "<what kind of action>", "command": "<what turtle does>" } --
--- JSON out format of { "type": "<what were sending back>", "up": "<what turtle sees up>", "down": "<what turtle sees down>", "forward": "<what turtle sees forward>" } --
+-- planned JSON format: 
+
 if ws then
         print("> connected")
-        parallel.waitForAny(listenForMessages, dataBack)            
-else
-        print("> error: " .. err)
-end
-
-local function listenForMessages()
-    while true do
-        
-        local message = ws.receive()
-        
-        if message then
-            local obj = textutils.unserializeJSON(message)
-           
-            if obj == nil then
-                print("Failed to parse JSON.")
-            else
-                print("Parsed JSON.")
-        
-                if obj.type == "eval" then
-                    if obj.command == "forward" then
-                        turtle.forward()
-                    elseif obj.command == "back" then
-                        turtle.back()
-                    elseif obj.command == "dig" then
-                        turtle.dig()
-                    elseif obj.command == "down" then
-                        turtle.down()
-                    elseif obj.command == "up" then
-                        turtle.up()
-                    elseif obj.command == "turnLeft" then
-                        turtle.turnLeft()
-                    elseif obj.command == "turnRight" then
-                        turtle.turnRight()
-                    else
-                        print("Unknown command: " .. tostring(obj.command))
-                    end
+        while true do
+                local message = ws.receive()
+                print("received raw message: " .. tostring(message))
+            
+                local obj = textutils.unserializeJSON(message)
+                
+                if obj == nil then
+                    print("Failed to parse JSON.")
                 else
-                    print("Unknown message type: " .. tostring(obj.type))
+                    print("Parsed JSON.")
+
+                    if obj.type == "eval" then
+                        if obj.command == "forward" then
+                            turtle.forward()
+                        elseif obj.command == "back" then
+                            turtle.back()
+                        elseif obj.command == "dig" then
+                            turtle.dig()
+                        elseif obj.command == "down" then
+                            turtle.down()
+                        elseif obj.command == "up" then
+                            turtle.up()
+                        elseif obj.command == "turnLeft" then
+                            turtle.turnLeft()
+                        elseif obj.command == "turnRight" then
+                            turtle.turnRight()
+                        else
+                            print("Unknown command: " .. tostring(obj.command))
+                        end
+                    elseif obj.type =="ping" then
+                        ws.send("pong")
+                    end
                 end
             end
-
-        end
-    end
-end
-
-local function dataBack()
-    
-    while true do
-        local up = turtle.inspectUp()
-        local down = turtle.inspectDown()
-        local forward = turtle.inspect()
-
-        local inspectData = {
-            type = "inspect",
-            up = up,
-            down = down,
-            forward = forward
-        }
-
-        local jsonData = textutils.serializeJSON(inspectData)
-
-        ws.send(jsonData)
-        sleep(1) -- 1 second delay between data updates
-
-    end
-
+            
+else
+        print("> error: " .. err)
 end
