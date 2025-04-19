6f7085aae23b40cc586973ac7ceb4a0e25931b47
igobyjac <136859155+igobyjac@users.noreply.github.com>
2025-04-19 09:01:42 -0500

    direction managment

--- turtle/startup.lua
+++ turtle/startup.lua
@@ -1,5 +1,26 @@
 --                  This is the script that must be inputted into the Turtle to establish the connection to the WebSocket                    --
 
+--turtles dont have a compass so direction it be tracked manually 
+--      1
+--  4       2
+--      3
+local direction = 1
+
+local function handleTurn(type)
+    if type == "left" then
+        direction =- 1
+        if direction < 1 then
+            direction = 4
+        end
+    elseif type == "right" then
+        direction += 1
+        if direction > 4 then
+            direction = 1
+        end
+    end
+end
+
+
 -- NGROK url --
 local ws, err = http.websocket("wss://<NGROK_URL>")
 
@@ -31,13 +52,32 @@ if ws then
                             turtle.up()
                         elseif obj.command == "turnLeft" then
                             turtle.turnLeft()
+                            handleTurn("left")
                         elseif obj.command == "turnRight" then
                             turtle.turnRight()
+                            handleTurn("right")
                         else
                             print("Unknown command: " .. tostring(obj.command))
                         end
+                    --we want data to be sent back to the server from the turtle
+                    --current JSON format: {type = "inspectResult", up={<INSPECT UP>, down=<INSPECT DOWN>, forward=<INSPECT FORWARD>}
                     elseif obj.type =="ping" then
-                        ws.send("pong")
+                        if obj.command =="test" then
+                            print("ping recieved")
+                            ws.send("pong")
+                        elseif obj.command =="inspect" then
+                            local up = turtle.inspectUp()
+                            local down = turtle.inspectDown()
+                            local forward = turtle.inspect()
+                            local data = {
+                                type = "inspectResult",
+                                up = up,
+                                down = down,
+                                forward = forward
+                            }
+                            local jsonData = textutils.serializeJSON(data)
+                            ws.send(jsonData)
+                        end
                     end
                 end
             end
